26d2f300a1baef1f50b49add29322d6b2d552372
x6v8fms6wr-hue <x6v8fms6wr@privaterelay.appleid.com>
2026-09-25 15:39:29 +0400

    mesele3

--- Main.java
+++ Main.java
@@ -1,19 +1,11 @@
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Birinci eded");
-        int a=sc.nextInt();
-
-        System.out.print("Ikinci eded");
-        int b = sc.nextInt();
-
-        System.out.print("Cem:" + (a + b));
-        System.out.print("Ferq:" + (a - b));
-        System.out.print("Hasil:" + (a * b));
-        System.out.print("Bolme:" + (double) a / b);
-        System.out.print("Qaliq:" + (a % b));
+        System.out.print("Selsi dərəcəsini daxil edin: ");
+        double c = sc.nextDouble();
+        double f = c * 9.0 / 5 + 32;
+        System.out.println("Fahrenheit: " + f);
     }
 }
