ef1a62acdc79e489187c6887ed7076c4990b41cd
x6v8fms6wr-hue <x6v8fms6wr@privaterelay.appleid.com>
2026-09-25 15:25:14 +0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Ədəd daxil edin: ");
-        int n = sc.nextInt();
-        if (n % 2 == 0) System.out.println(n + " cütdür.");
-        else System.out.println(n + " təkdir.");
+        System.out.print("Selsi dərəcəsini daxil edin: ");
+        double c = sc.nextDouble();
+        double f = c * 9.0 / 5 + 32;
+        System.out.println("Fahrenheit: " + f);
     }
 }
